bee9a2993705336cce6ea4676a5efdad477d6b15
pedkfuri <pedrokfuri@outlook.com>
2025-03-14 02:59:18 +0000

    readme & todo

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ PORT=5000
 
 HOSTNAME=localhost
 
+GITLAB_HOST=gitlab.com
+
+WEBHOOK_URL=https://eff1-2804-1b3-61c1-3682-8586-de03-f190-dd1d.ngrok-free.app/webhook
+
 OLLAMA_API=192.168.15.92:88
 
 
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ createGitlabWebhook(68005822, process.env.WEBHOOK_URL);
 // createGithubWebhook(process.env.WEBHOOK_URL);
 
 //TODO: check if incoming event on /webhook is an MR being open, if not, return
+//TODO: implement choose strategy (gitlab || github) when /webhook is requested 
 //TODO: choose service when first running (app args = gitlab || github)
 //TODO: receber o ID de projeto como variavel de ambiente ao executar container/app
 //TODO: modularize
